5d31ef5549889a95286866f79af170fc443b5bc5
Ivan Rostovsky <ir@anahoret.com>
2011-11-08 11:32:26 +0200

    Added simple articles

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::MultiParameterAttributes
+  field :name
+  field :content
+  field :published_on, :type => Date
+  validates_presence_of :name
+end
