73554217ceefef2274960b5fd1f01f68d2ddcb68
Ivan Rostovsky <ir@anahoret.com>
2011-11-18 16:53:12 +0200

    Clean up

--- doc/refactor_test_completed.rb
+++ doc/refactor_test_completed.rb
@@ -1,29 +1,12 @@
 class OfficesController < InheritedResources::Base
-  # NOTE: This application uses InheritedResources
-  # What is happening below:
-  # We require a list of near by offices (within 6 miles) of a long/lat location
-  # Location can be obtained 3 different ways: [Order of lookup is important]
-  # 1) Location is sent via params
-  # 2) Location is gotten using IP and lookup an IP database (get_ip_based_location)
-  # 3) Default to a specific location (get_city_hall_offices)
-  # 
-  # No need to worry about the rendering, just that @offices should return a list of near by offices
-  # 
-  # Task: Refactor this code to a cleaner and easier ot read code.
-  # _IF_ you need ot move something into the office model, add it to the Office class at the bottom
-  # of this file. 
-  
-  
   def getoffices
     latitude = params[:latitude]
     longitude = params[:longitude]
     @offices = end_of_association_chain.near_by(latitude, longitude, {:remote_ip => request.remote_ip}).page(params[:page])
   end
 
+protected
 
-  #########
-  protected
-  #########
   def collection
     latitude = cookies[:latitude]
     longitude = cookies[:longitude]
